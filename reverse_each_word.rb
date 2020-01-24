3f01d98193d622fb5d81370a9c8b3c63d5437bcf
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 06:03:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_arr = string.reverse.split(/ /)
   new_arr = string_arr.reverse
-  yield(new_arr)
+  new_arr.each do |new_arr|
+    new_arr.join(" ")
+  end
 end
-
-reverse_each_word.each("s") { |s| s.join(" ")}
